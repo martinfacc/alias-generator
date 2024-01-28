a78f9f790b6f4af73d4b966aabeeb947a649c20f
Martin Facciuto <martin_facciuto@hotmail.com>
2024-01-28 10:53:09 -0300

    Fix bug in login functionality Add validation for user input Update error handling in API calls Refactor code for better readability Optimize database queries Update README with new instructions Add unit tests for new features Fix formatting issues in code Update dependencies to latest versions Implement new feature: user profile picture upload

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "alias-generator", // Un nombre para tu aplicación
+      script: "serve",
+      env: {
+        PM2_SERVE_PATH: "./dist", // El directorio de los archivos construidos por Vite
+        PM2_SERVE_PORT: 3609, // El puerto en el que tu aplicación será accesible
+        PM2_SERVE_SPA: "true", // Servir la aplicación como una Single Page Application
+        PM2_SERVE_HOMEPAGE: "/index.html", // Página principal para SPA
+      },
+    },
+  ],
+};
